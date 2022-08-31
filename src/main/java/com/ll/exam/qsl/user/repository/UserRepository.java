2e9cc713c7062a40a331199abb8b66104ab26960
jinsu291 <wlstn291@naver.com>
2022-09-01 01:00:16 +0900

    QueryDsl 로직용 클래스 UserRepositoryImpl 추가

--- src/main/java/com/ll/exam/qsl/user/repository/UserRepository.java
+++ src/main/java/com/ll/exam/qsl/user/repository/UserRepository.java
@@ -3,6 +3,6 @@ package com.ll.exam.qsl.user.repository;
 import com.ll.exam.qsl.user.entity.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<SiteUser, Long> {
+public interface UserRepository extends JpaRepository<SiteUser, Long>, UserRepositoryCustom {
 
 }
