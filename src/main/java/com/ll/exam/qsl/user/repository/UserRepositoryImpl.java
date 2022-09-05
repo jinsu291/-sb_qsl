3da8373e0017bbb5f898822c0573d8ef6a06d965
jinsu291 <wlstn291@naver.com>
2022-09-05 09:27:12 +0900

    sort 처리

--- src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryImpl.java
@@ -2,10 +2,15 @@ package com.ll.exam.qsl.user.repository;
 
 import com.ll.exam.qsl.user.entity.QSiteUser;
 import com.ll.exam.qsl.user.entity.SiteUser;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.support.PageableExecutionUtils;
 
 import java.util.List;
@@ -69,7 +74,7 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
 
     @Override
     public Page<SiteUser> searchQsl(String kw, Pageable pageable) {
-        List<SiteUser> users = jpaQueryFactory
+        JPAQuery<SiteUser> usersQuery = jpaQueryFactory
                 .select(siteUser)
                 .from(siteUser)
                 .where(
@@ -77,9 +82,14 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
                                 .or(siteUser.email.contains(kw))
                 )
                 .offset(pageable.getOffset()) // 몇개를 건너 띄어야 하는지 LIMIT {1}, ?
-                .limit(pageable.getPageSize()) // 한페이지에 몇개가 보여야 하는지 LIMIT ?, {1}
-                .orderBy(siteUser.id.asc())
-                .fetch();
+                .limit(pageable.getPageSize()); // 한페이지에 몇개가 보여야 하는지 LIMIT ?, {1}
+
+        for (Sort.Order o : pageable.getSort()) {
+            PathBuilder pathBuilder = new PathBuilder(siteUser.getType(), siteUser.getMetadata());
+            usersQuery.orderBy(new OrderSpecifier<>(o.isAscending() ? Order.ASC : Order.DESC, pathBuilder.get(o.getProperty())));
+        }
+
+        List<SiteUser> users = usersQuery.fetch();
 
         LongSupplier totalSupplier = () -> 2;
 
