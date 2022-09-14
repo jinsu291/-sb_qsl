cc1ade0b37c3810cdddb6c605ba9a9da67e908ce
jinsu291 <wlstn291@naver.com>
2022-09-14 11:26:05 +0900

    본인은 본인을 follow 할 수 없다.

--- src/main/java/com/ll/exam/qsl/user/controller/UserController.java
+++ src/main/java/com/ll/exam/qsl/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.ll.exam.qsl.user.controller;
 import com.ll.exam.qsl.user.entity.SiteUser;
 import com.ll.exam.qsl.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class UserController {
     private final UserRepository userRepository;
 
     @RequestMapping("/user/{id}")
+    @Transactional
     public SiteUser user(@PathVariable Long id) {
         return userRepository.getQslUser(id);
     }
--- src/test/java/com/ll/exam/qsl/UserRepositoryTests.java
+++ src/test/java/com/ll/exam/qsl/UserRepositoryTests.java
@@ -251,4 +251,15 @@ class UserRepositoryTests {
 
 		userRepository.save(u2);
 	}
+
+	@Test
+	@DisplayName("본인이 본인을 follow 할 수 없다.")
+	@Rollback(false)
+	void t14() {
+		SiteUser u1 = userRepository.getQslUser(1L);
+
+		u1.follow(u1);
+
+		assertThat(u1.getFollowers().size()).isEqualTo(0);
+	}
 }
