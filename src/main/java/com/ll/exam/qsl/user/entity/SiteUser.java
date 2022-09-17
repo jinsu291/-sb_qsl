a424a4a9dddd5ed11ce7cbb09b77dd5bac387fb6
jinsu291 <wlstn291@naver.com>
2022-09-17 14:31:53 +0900

    정답

--- src/main/java/com/ll/exam/qsl/user/entity/SiteUser.java
+++ src/main/java/com/ll/exam/qsl/user/entity/SiteUser.java
@@ -4,7 +4,6 @@ import com.ll.exam.qsl.interestKeyword.entity.InterestKeyword;
 import lombok.*;
 
 import javax.persistence.*;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -28,7 +27,7 @@ public class SiteUser {
     private String email;
 
     @Builder.Default
-    @ManyToMany(cascade = CascadeType.ALL)
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     private Set<InterestKeyword> interestKeywords = new HashSet<>();
 
     @Builder.Default
@@ -39,9 +38,8 @@ public class SiteUser {
     @ManyToMany(cascade = CascadeType.ALL)
     private Set<SiteUser> followings = new HashSet<>();
 
-
     public void addInterestKeywordContent(String keywordContent) {
-        interestKeywords.add(new InterestKeyword(keywordContent));
+        interestKeywords.add(new InterestKeyword(this, keywordContent));
     }
 
     public void follow(SiteUser following) {
@@ -52,7 +50,7 @@ public class SiteUser {
         // 유튜버(following)이 나(follower)를 구독자로 등록
         following.getFollowers().add(this);
 
-    // 내(follower)가 유튜버(following)를 구독한다.
-    getFollowings().add(following);
+        // 내(follower)가 유튜버(following)를 구독한다.
+        getFollowings().add(following);
     }
 }
